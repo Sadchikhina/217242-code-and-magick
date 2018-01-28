cc743a43dbf17323ef5137edf13825c970376ca3
Varvara Sadchikhina <sadchikhina@gmail.com>
2018-01-28 13:14:56 +0300

    bars added

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,7 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var BAR_WIDTH = 40;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -16,4 +17,23 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = 'bold 16px PT Mono';
   ctx.fillText('Ура вы победили!', 180, 30);
   ctx.fillText('Список результатов:', 180, 50);
+
+  ctx.fillText('Вы', 140, 260);
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(140, 80, BAR_WIDTH, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вова', 230, 260);
+  ctx.fillStyle = '#0033ff';
+  ctx.fillRect(230, 80, BAR_WIDTH, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Маша', 320, 260);
+  ctx.fillStyle = '#0033ff';
+  ctx.fillRect(320, 80, BAR_WIDTH, 150);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Cwikla', 410, 260);
+  ctx.fillStyle = '#0033ff';
+  ctx.fillRect(410, 80, BAR_WIDTH, 150);
 };
