4819352a77f4c10ed0e364cecb558bababc5967c
Varvara Sadchikhina <sadchikhina@gmail.com>
2018-01-28 11:58:49 +0300

    cloud upgraded

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,14 @@
 'use strict';
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
 window.renderStatistics = function (ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 10, 420, 270);
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7');
+  renderCloud(ctx, 100, 20, '#fff');
 };
