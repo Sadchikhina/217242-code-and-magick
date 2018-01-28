82bf3d27387ab4bf90daeb5555e812b63afea456
Varvara Sadchikhina <sadchikhina@gmail.com>
2018-01-28 12:33:38 +0300

    text added

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,12 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7');
-  renderCloud(ctx, 100, 20, '#fff');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Ура вы победили!', 180, 30);
+  ctx.fillText('Список результатов:', 180, 50);
 };
